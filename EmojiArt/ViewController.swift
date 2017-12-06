87419a413963c95b152ee1cc709d7220d4b3109f
johnrm9 <johnrm9@gmail.com>
2017-12-06 08:29:19 -0800

    Initial Commit

---
+++ EmojiArt/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  EmojiArt
+//
+//  Created by John Martin on 12/6/17.
+//  Copyright © 2017 John Martin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
+
