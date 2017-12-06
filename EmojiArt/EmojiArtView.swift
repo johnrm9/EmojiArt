cf5fd6f509b796aa2fb1096049b8786f9346e8cb
johnrm9 <johnrm9@gmail.com>
2017-12-06 08:39:08 -0800

    Created custom UIView - EmojiArtView.

---
+++ EmojiArt/EmojiArtView.swift
@@ -0,0 +1,21 @@
+//
+//  EmojiArtView.swift
+//  EmojiArt
+//
+//  Created by John Martin on 12/6/17.
+//  Copyright © 2017 John Martin. All rights reserved.
+//
+
+import UIKit
+
+class EmojiArtView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ EmojiArt/EmojiArtViewController.swift
@@ -0,0 +1,29 @@
+//
+//  EmojiArtViewController.swift
+//  EmojiArt
+//
+//  Created by John Martin on 12/6/17.
+//  Copyright © 2017 John Martin. All rights reserved.
+//
+
+import UIKit
+
+class EmojiArtViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .purple
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
